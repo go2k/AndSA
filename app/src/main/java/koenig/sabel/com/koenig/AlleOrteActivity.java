7afc9435283ad25a9ae62a9fd523fbf70b208a05
Michael König <m.koenig@go2k.de>
2018-05-03 08:25:34 +0200

    ...

---
+++ app/src/main/java/koenig/sabel/com/koenig/AlleOrteActivity.java
@@ -0,0 +1,13 @@
+package koenig.sabel.com.koenig;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlleOrteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alle_orte);
+    }
+}
